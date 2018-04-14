1616647d00aa975b20efcff623ad364134b45a59
zachha <zachha@gmail.com>
2018-04-14 17:26:16 -0400

    moved towards traditional MVC framework, handlebars and route file added

---
+++ controllers/api.js
@@ -0,0 +1,71 @@
+const cheerio = require('cheerio');
+const request = require('request');
+const express = require('express');
+const router = express.Router();
+const db = require('../models');
+
+
+// GET to scrape The Washington Post
+// BUG with the scrape function, not doing what it needs to.
+router.get("/scrape", (req, res) => {
+  //html body is requested
+  console.log("WORDS");
+  request("http://www.medium.com/topic/javascript/", (error, reply) => {
+    console.log("inside scrape");
+    //html loads into cheerio
+    const $ = cheerio.load(reply.data);
+    let result = {};
+    // grabs the title and link for each post on the subreddit
+    $("div.js-trackedPost").each(function(i, element) {
+      reply.category = "Javascript";
+      //reply.title = $(this).children().children('div').children();
+      reply.title = $(this)
+        .find("a.u-block")
+        .attr("aria-label");
+      //reply.link = $(this).children().children("a").attr("href");
+      reply.link = $(this)
+        .find("a.u-block")
+        .attr("href");
+      //reply.summary = $(this).children().children("div").children("div.flex1").children().children("a").children()
+      reply.summary = $(this)
+        .find("h4.ui-summary")
+        .text();
+      //new Article created in the db using reply obj
+      db.Article.create(reply)
+        .then(function(dbArticle) {
+          // logs the article added
+          console.log(dbArticle);
+          res.json(dbArticle);
+        })
+        .catch(err => res.json(err));
+    });
+  });
+});
+
+// will search the articles db and populate articles and notes from database
+router.get("/articles", (req, re) => {
+  db.Article.find({})
+    .then(dbArticle => {
+      res.json(dbArticle);
+    })
+    .catch(err => {
+      res.json(err);
+    });
+});
+
+//route for grabbing one article and showing it's notes
+router.get("/articles/:id", (req, res) => {
+  db.Article.findOne({ _id: req.params.id })
+    .populate("note")
+    .then(function(dbArticle) {
+      res.json(dbArticle);
+    })
+    .catch(err => res.json(err));
+});
+
+/*
+// add route for adding article note
+router.post("/articles/:id", (req, res) => {
+  db.Note.
+})
+*/
--- server.js
+++ server.js
@@ -7,14 +7,24 @@ const cheerio = require('cheerio');
 const handlebars = require('express-handlebars');
 const PORT = process.env.PORT || 8000;
 
+//models linked
 const db = require("./models");
-
+// express initialized
 const app = express();
 
 // used to handle form submissions
 app.use(bodyParser.urlencoded({ extended: true }));
+
+// handlebars added
+const exphbs = require('express-handlebars');
+app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
+app.set('view engine', 'handlebars');
+
 // used to serve public folder as static directory
 app.use(express.static('public'));
+//Controllers added
+const router = require('./controllers/api.js');
+app.use(router);
 
 // If deployed, use the deployed database. Otherwise use the local mongoHeadlines database
 var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
@@ -24,46 +34,8 @@ var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines
 mongoose.Promise = Promise;
 mongoose.connect(MONGODB_URI, {});
 
-// GET to scrape The Washington Post
-app.get("/scrape", (req, res) => {
-  //html body is requested
-  console.log("WORDS");
-  axios.get("http://www.medium.com/topic/javascript/").then((reply) => {
-    console.log("inside scrape");
-    //html loads into cheerio 
-    const $ = cheerio.load(reply.data);
-    let result = {};
-    // grabs the title and link for each post on the subreddit
-    $("div.js-trackedPost").each(function(i, element) {
-      reply.category = "Javascript";
-      //reply.title = $(this).children().children('div').children();
-      reply.title = $(this).find("a.u-block").attr("aria-label");
-      //reply.link = $(this).children().children("a").attr("href");
-      reply.link = $(this).find("a.u-block").attr("href");
-      //reply.summary = $(this).children().children("div").children("div.flex1").children().children("a").children()
-      reply.summary = $(this).find("h4.ui-summary").text();
-    //new Article created in the db using reply obj
-    db.Article.create(reply)
-      .then(function(dbArticle) {
-      // logs the article added
-        console.log(dbArticle);
-        res.json(dbArticle);
-      }).catch( err => res.json(err));
-    });
-
-  });
-});
-
-app.get("/articles", (req, re) => {
-  db.Article.find({})
-    .then( (dbArticle) => {
-      res.json(dbArticle);
-    })
-    .catch( (err) => {
-      res.json(err);
-    });
-});
 
 app.listen(PORT, function() {
   console.log("App listening on PORT " + PORT);
 });
+
