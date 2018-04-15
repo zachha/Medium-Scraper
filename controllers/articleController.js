6f3c2754eebfcabaf38daf3e9fe468694d5d8036
zachha <zachha@gmail.com>
2018-04-15 17:48:26 -0400

    deleteNote, showNote, createNote functions added to controller

--- controllers/articleController.js
+++ controllers/articleController.js
@@ -11,5 +11,43 @@ module.exports = {
         });
     },
 
-    
+    addArticle: (result) => {
+        db.Article.create(result)
+          .then((dbArticle) => {
+            // logs the article added
+            console.log(dbArticle);
+            res.json(dbArticle);
+          })
+          .catch(err => res.json(err));
+    },
+
+    showNotes: (thisId) => {
+        db.Article.findOne({ _id: thisId })
+          .populate("note")
+          .then((dbArticle) => {
+            res.json(dbArticle);
+          })
+          .catch(err => res.json(err));
+    },
+
+    createNote: (thisId, noteText) => {
+        db.Note.create(noteText)
+        .then((dbNote) => {
+        // associates note with article
+          return db.Article.findOneAndUpdate({ _id: req.params.id }, { note: dbNote._id }, { new: true });
+        })
+        .then((dbArticle) => {
+            res.json(dbArticle);
+        })
+        .catch((err) => {
+            res.json(err);
+        });
+    },
+
+    deleteNote: (thisId) => {
+        db.Note.deleteOne({
+            _id: thisId
+        })
+        .then
+    }
 }
