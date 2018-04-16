51bcd13f1eb9d086a8194bd48a90affa60b6a007
zachha <zachha@gmail.com>
2018-04-16 00:53:27 -0400

    home and save buttons and routes complete

--- controllers/api.js
+++ controllers/api.js
@@ -1,10 +1,12 @@
 const cheerio = require('cheerio');
 const request = require('request');
 const express = require('express');
+const bodyParser = require("body-parser");
 const router = express.Router();
-const adb = require("../articleController");
+// links the model controller
+const adb = require("./articleController");
 
-
+// GET route for main page, populate sthe titles appropriately
 router.get("/", (req, res) => {
     let hbsObj = { 
         mainTitle: "Medium Article Scraper",
@@ -12,32 +14,29 @@ router.get("/", (req, res) => {
     };
     res.render("index", hbsObj);
 })
+
 // GET to scrape The Washington Post
-// BUG with the scrape function, not doing what it needs to.
 router.get("/scrape", (req, res) => {
   //html body is requested
   request("http://www.medium.com/topic/javascript/", (error, response, html) => {
     //html loads into cheerio
     const $ = cheerio.load(html);
     let result = {};
-    // grabs the title and link for each post on the subreddit
+    // grabs the title and link for each article 
     $("div.js-trackedPost").each(function(i, element) {
       result.category = "Javascript";
-      //reply.title = $(this).children().children('div').children();
       result.title = $(this)
         .find("a.u-block")
         .attr("aria-label");
-      //reply.link = $(this).children().children("a").attr("href");
       result.link = $(this)
         .find("a.u-block")
         .attr("href");
-      //reply.summary = $(this).children().children("div").children("div.flex1").children().children("a").children()
       result.summary = $(this)
         .find("h4.ui-summary")
         .text();
       //new Article created in the db using reply obj
       if((result.link) && (result.title)) {
-        // ADD ARTICLE
+        adb.addArticle(result);
       }
     });
   });
@@ -50,25 +49,37 @@ router.get("/articles", (req, res) => {
 
 //route for grabbing one article and showing it's notes
 router.get("/articles/:id", (req, res) => {
-  // SHOW NOTES OF ARTICLE
   let thisId = req.params.id;
+  adb.showNotes(thisId);
 });
 
+//route to update an article to be Saved
+router.put("/articles/:id", (req, res) => {
+    let thisId = req.params.id;
+    adb.saveArticle(thisId);
+})
 
-// add route for adding article note
+//route to update an article to unsave it
+router.put("/articles/:id", (req, res) => {
+    let thisId = req.params.id;
+    adb.unsaveArticle(thisId);
+})
+
+// route for adding article note
 router.post("/articles/:id", (req, res) => {
     let thisId = req.params.id;
     let noteText = req.body;
-  // ADD NOTE 
+    adb.createNote(thisId, noteText);
 })
 
-
+// finds all saved articles
 router.get("/saved", (req, res) => {
     let hbsObj = { 
         mainTitle: "Saved Articles",
         subtitle: "Click the articles to view comments!"
     };
-    res.render('saved', hbsObj);
+    res.render('index', hbsObj);
+    adb.findSaved();
 })
 
 module.exports = router;
--- public/app.js
+++ public/app.js
@@ -1,12 +1,12 @@
 $(document).ready(() => {
-    document.getElementById("scrape").disabled = false;
+    document.getElementById("scrapeBtn").disabled = false;
 
     function addArticles(data)  {
-        data.forEach( (data) => {
-            $("#articles").append(`<div class="card">
+        data.forEach( (data, articleDiv) => {
+            $("#" + articleDiv).append(`<div class="card">
                     <h5 class="card-header" data-id=${data._id}>${data.category}</h5>
                     <div class="card-body">
-                    <h5 class="card-title">${data.title}<button class="btn btn-primary saveBtn">Save Article</button></h5>
+                    <h5 class="card-title">${data.title}<button class="btn btn-primary saveArticle">Save Article</button></h5>
                     <p class="card-text">${data.summary}</p>
                     <a href="${data.link}" class="btn btn-primary" target="_blank">Link to Article</a>
                     </div>
@@ -15,27 +15,51 @@ $(document).ready(() => {
     );
     }
 
-//$.getJSON("/articles", addArticles(res));
-
-$("#scrapeBtn").on('click', () => {
-    console.log("hi");
-    $.ajax({ method: "GET",
-             url: "/scrape",
-             dataType: "json" })
-      .done((data) => {
+    // On click event for Scrape, scrapes Medium for new articles and adds them to the database if they aren't in it then populates cards for each article
+    $("#scrapeBtn").on("click", () => {
+      console.log("hi");
+      $.ajax({ method: "GET", url: "/scrape", dataType: "json" })
+        .done(data => {
           console.log("scrape done, pulling from db!");
-          $.get("/articles", (data) => {
+          $.get("/articles", data => {
             console.log(data);
-            addArticles(data);
+            addArticles(data, articles);
           })
-          .done( () => console.log("db pull complete!"))
-          .fail( err => console.log(err) );
-      })
-      .fail(err => console.log(err));
-});
+            .done(() => console.log("db pull complete!"))
+            .fail(err => console.log(err));
+        })
+        .fail(err => console.log(err));
+    });
 
-$("#saveBtn").on('click', () => {
-    
-})
+    // On click event for Save button, adjusts titles and populates saved articles, disables scrape button
+    $("#saveBtn").on("click", () => {
+      $("#articles").empty();
+      $.get("/saved", data => {
+        console.log("saved articles: " + data);
+        addArticles(data, savedArticles);
+        document.getElementById("scrapeBtn").disabled = true;
+      });
+    });
+
+    // On click event for Home button, resets page back to '/' route and populates unsaved articles from db
+    $("#home").on("click", () => {
+        $("#savedArticles").empty();
+        $.get("/", (req, res) => {})
+        .then( () => {
+            $.get("/articles", data => {
+              console.log(data);
+              addArticles(data, articles);
+            })
+              .done(() => console.log("db pull complete!"))
+              .fail(err => console.log(err));
+        })
+        .fail( err => console.log(err) );
+    })
+
+    $(".saveArticle").on('click', () => {
+        $.put("/save")
+    })
+
+
 
 });
