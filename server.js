953218a422a0c68bfeca9b5331460802300b36be
zachha <zachha@gmail.com>
2018-04-19 00:38:33 -0400

    fixed server database deployment code

--- server.js
+++ server.js
@@ -32,7 +32,9 @@ var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines
 // Set mongoose to leverage built in JavaScript ES6 Promises
 // Connect to the Mongo DB
 mongoose.Promise = Promise;
-mongoose.connect(MONGODB_URI, {});
+mongoose.connect(MONGODB_URI, {
+  useMongoClient: true
+});
 
 // listening for server
 app.listen(PORT, function() {
